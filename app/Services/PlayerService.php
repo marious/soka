3285d6f548cef803668d3ea7c76dbc3b3b072bbd
Mohamed Raya <mohamedraya410@gmail.com>
2023-09-06 01:28:58 +0300

    fix-issues

--- app/Services/PlayerService.php
+++ app/Services/PlayerService.php
@@ -34,19 +34,23 @@ class PlayerService
             })
             ->when(isset($search['searchItems']) && isset($search['searchItems']['sort_by']), function ($query) use ($search) {
                 $sortBy =  $search['searchItems']['sort_by'] == 'age' ? 'dob': $search['searchItems']['sort_by'];
+                if ($sortBy == 'dob') {
+                    $search['searchItems']['sort_type'] =  $search['searchItems']['sort_type'] == 'asc' ? 'desc' : 'asc';
+                }
                 $query->orderBy($sortBy, $search['searchItems']['sort_type']);
             });
 
         foreach ($searchItems as $key => $value) {
             if ($key === 'age') {
                 $query->whereRaw('(DATEDIFF("' . date('Y-m-d') . '",dob)/365) >= ' . $value)
-                    ->when(! request()->has('sort_by'), function ($query) {
-                        $query->orderBy('dob', 'ASC');
+                    ->when(request()->has('sort_by') && request('sort_by') == 'age' && request('sort_type') == 'asc',
+                        function ($query) {
+                        $query->orderBy('dob', 'DESC');
                     });
             } else {
                 $query->where($key, '>=', $value)
                     ->when(! request()->has('sort_by'), function ($query) use ($key) {
-                        $query->orderBy('key', 'DESC');
+                        $query->orderBy('key', 'ASC');
                     });
             }
 
